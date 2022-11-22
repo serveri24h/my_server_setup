e59b1b168358cf27b61a204db7ff800e3697e8df
serveri24h <severi.vapalahti@gmail.com>
2022-11-23 01:42:05 +0200

    server setup for debian based linode server

---
+++ setup_server.sh
@@ -0,0 +1,72 @@
+########################################
+####            SETUP               ####
+########################################
+
+#update package manager
+apt update && apt upgrade 
+apt install build-essential
+apt install htop
+
+
+########################################
+####            USERS               ####
+########################################
+
+#make adminuser
+adduser admin
+usermod -a -G sudo admin
+su admin
+
+
+#########################################
+###            TOOLS                  ###
+#########################################
+
+#rust
+echo "Install rust? (y/N)"
+read install_rust
+if [$install_rust=="y"]; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+#install node.js
+echo "Install rust? (y/N)"
+read install_node
+if [$install_node=="y"]; then
+    curl -fsSL https://deb.nodesource.com/setup_19.x | bash - &&\
+    apt-get install -y nodejs
+fi
+
+
+#########################################
+####         SERVER SETUP            ####
+#########################################
+
+echo "Setup server with nginx? (y/N)"
+read setup_server
+if [$setup_server=="y"] then
+    curl -fsSL https://deb.nodesource.com/setup_19.x | bash - &&\
+    apt-get install -y nodejs
+
+    #make folder for the project
+    mkdir -p /var/www/mydomain.com
+    chmod 755 -R /var/www/mydomain.com
+
+    #install rsync
+    apt install rsync
+
+    # install, setup and start nginx
+    apt install nginx -y
+    echo "server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/mydomain.com; 
+            index index.html index.htm;
+
+    }
+    " > /etc/nginx/conf.d/myapp.conf
+    systemctl restart nginx
+fi
+
+
