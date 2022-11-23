a9251a174f6a355d23f9d80a65055c6094857360
serveri24h <severi.vapalahti@gmail.com>
2022-11-23 02:00:40 +0200

    spitting the code up after and before user change

--- setup_server.sh
+++ setup_server.sh
@@ -2,10 +2,12 @@
 ####            SETUP               ####
 ########################################
 
+## First update and uprade apt and download git
+##       -> then clone this repo
+
 #update package manager
-apt update && apt upgrade 
-apt install build-essential
-apt install htop
+apt install build-essential -y
+apt install htop -y
 
 
 ########################################
@@ -18,55 +20,6 @@ usermod -a -G sudo admin
 su admin
 
 
-#########################################
-###            TOOLS                  ###
-#########################################
 
-#rust
-echo "Install rust? (y/N)"
-read install_rust
-if [$install_rust=="y"]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-fi
-
-#install node.js
-echo "Install rust? (y/N)"
-read install_node
-if [$install_node=="y"]; then
-    curl -fsSL https://deb.nodesource.com/setup_19.x | bash - &&\
-    apt-get install -y nodejs
-fi
-
-
-#########################################
-####         SERVER SETUP            ####
-#########################################
-
-echo "Setup server with nginx? (y/N)"
-read setup_server
-if [$setup_server=="y"] then
-    curl -fsSL https://deb.nodesource.com/setup_19.x | bash - &&\
-    apt-get install -y nodejs
-
-    #make folder for the project
-    mkdir -p /var/www/mydomain.com
-    chmod 755 -R /var/www/mydomain.com
-
-    #install rsync
-    apt install rsync
-
-    # install, setup and start nginx
-    apt install nginx -y
-    echo "server {
-        listen 80;
-        listen [::]:80;
-
-        root /var/www/mydomain.com; 
-            index index.html index.htm;
-
-    }
-    " > /etc/nginx/conf.d/myapp.conf
-    systemctl restart nginx
-fi
 
 
