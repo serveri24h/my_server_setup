d9e714340a719f7894a16a6c09f6866c2cd059fb
serveri24h <severi.vapalahti@gmail.com>
2022-12-02 22:15:05 +0200

    minor fixes

--- change_to_key.sh
+++ change_to_key.sh
@@ -5,7 +5,7 @@ if [ "$EUID" == 0 ]; then
     # (sudo) ssh-copy-id <user@addres>
 
     python3 tools/edit_sshd_config.py pswd_log
+    systemctl restart ssh
 else
     echo " This script needs to be run with sudo privilidges."
-
 fi
